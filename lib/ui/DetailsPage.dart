5476835be8e03f183f3992b6bd7823dabc25ec6a
Syed Piyal <piyalhenry@gmail.com>
2024-02-05 18:56:53 +0600

    Json data show in listview

--- lib/ui/DetailsPage.dart
+++ lib/ui/DetailsPage.dart
@@ -3,12 +3,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class DetailsPage extends StatelessWidget {
+
+class DetailsPage extends StatefulWidget {
   //const DetailsPage({super.key});
 
   const DetailsPage({super.key, required this.dataModel});
 
   final DataModel dataModel;
+
+
+
+  @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState  extends State<DetailsPage>{
+  int selectedOption = 1;
+
+  String dropdownValue = 'Good';
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +31,7 @@ class DetailsPage extends StatelessWidget {
           Container(
             height: 300,
             child: Image.asset(
-              dataModel.image!,
+              widget.dataModel.image!,
               fit: BoxFit.cover,
             ),
           ),
@@ -29,12 +41,12 @@ class DetailsPage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  dataModel.title!,
+                  widget.dataModel.title!,
                   style: Theme.of(context).textTheme.headline2,
                 ),
                 RatingBar.builder(
                   ignoreGestures: true,
-                  initialRating: dataModel.rating!,
+                  initialRating: widget.dataModel.rating!,
                   minRating: 1,
                   direction: Axis.horizontal,
                   allowHalfRating: true,
@@ -57,12 +69,12 @@ class DetailsPage extends StatelessWidget {
             child: Row(
               children: [
                 Image.asset(
-                  dataModel.location!,
+                  widget.dataModel.location!,
                   width: 20,
                   height: 20,
                 ),
                 const SizedBox(width: 8),
-                Text(dataModel.address!),
+                Text(widget.dataModel.address!),
               ],
             ),
           ),
@@ -71,15 +83,16 @@ class DetailsPage extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(dataModel.ref!),
+                Text(widget.dataModel.ref!),
                 Text(
-                  dataModel.price!,
+                  widget.dataModel.price!,
                   style: Theme.of(context).textTheme.bodyText1,
                 ),
               ],
             ),
           ),
 
+          //button for booking and alert dialog
           Padding(
             padding: const EdgeInsets.only(left: 40,right: 40,top: 50),
             child: ElevatedButton(
@@ -87,16 +100,70 @@ class DetailsPage extends StatelessWidget {
                 showDialog(
                   context: context,
                   builder: (ctx) => AlertDialog(
+                    title:  const Text("Are You Happy With the Design ?",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold)),
+                    content: Container(
+                      height: 200,
+                      child: Column(
+                        //mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          ListTile(
+                            title: const Text('Option 1'),
+                            leading: Radio(
+                              value: 1,
+                              groupValue: selectedOption,
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedOption = value!;
+                                });
+                              },
+                            ),
+                          ),
+                          ListTile(
+                            title: const Text('Option 2'),
+                            leading: Radio(
+                              value: 2,
+                              groupValue: selectedOption,
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedOption = value!;
+                                });
+                              },
+                            ),
+                          ),
 
-                    title:  Text("Are You Happy With the Design ?",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold)),
-                    content: const Text("You have raised a Alert Dialog Box"),
+                          // Step 2.
+                          DropdownButton<String>(
+                            // Step 3.
+                            value: dropdownValue,
+                            // Step 4.
+                            items: <String>['Good', 'Not Good', 'Bad', 'Best']
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: TextStyle(fontSize: 30),
+                                ),
+                              );
+                            }).toList(),
+                            // Step 5.
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownValue = newValue!;
+                              });
+                            },
+                          ),
+
+                        ],
+                      ),
+                    ),
                     actions: <Widget>[
                       TextButton(
                         onPressed: () {
                           Navigator.of(ctx).pop();
                         },
                         child: Container(
-                          color: Colors.green,
+                          color: Colors.deepOrangeAccent,
                           padding: const EdgeInsets.all(14),
                           child: const Text("okay"),
                         ),
@@ -121,6 +188,12 @@ class DetailsPage extends StatelessWidget {
               child: const Text("Book",style: TextStyle(fontSize: 15)),
             ),
           ),
+      Padding(
+        padding: const EdgeInsets.only(left: 150,top: 50),
+        child:Text(dropdownValue,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)
+      ),
+          
+          
         ],
       ),
     );
